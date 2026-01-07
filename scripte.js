110f693362b1757536c9c92748cd04aef1f5e5a6
mashood7 <mashoodayub7@gmail.com>
2026-01-07 15:25:37 +0500

    e project

---
+++ scripte.js
@@ -0,0 +1,5 @@
+ <script>
+        function openPrice(name, price) 
+            alert("Product: " + name + "\nPrice: " + price + "\n\nPlease fill the contact form to buy!");
+        
+    </script>
